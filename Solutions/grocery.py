7ba0a18461e8006703ad4efa6999a0c91001e41b
westerdog <westerlund.lauri@gmail.com>
2023-04-26 01:16:53 +0300

    Update grocery.py

--- Solutions/grocery.py
+++ Solutions/grocery.py
@@ -7,14 +7,14 @@ grocerylist = {}                                        # Define the grocery lis
             item = input().upper()                      # Ask for an item
 
             if item not in grocerylist:                 # If the item isn't on the list already,
-                grocerylist[item] = 1                   # put it there and set its value (amount) to 1
+                grocerylist[item] = 1                   # put it there and set its amount (value) to 1
             else:
-                grocerylist[item] += 1                  # Otherwise it's already there, so increase its value by 1.
+                grocerylist[item] += 1                  # Otherwise it's already there, so increase its amount by 1.
 
         except EOFError:                                # When the user ends input
 
             for n in sorted(grocerylist.keys()):        # Sort the grocery list items (keys) alphabetically, and iterate through the items
-                print(grocerylist[n], n)                # Print the value (amount) and then the name of the item (key)
+                print(grocerylist[n], n)                # Print the amount and then the name of the item
                 
             break                                       # Then stop
 
